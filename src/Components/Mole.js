8f8447a95f7117054eea634284a1700500c12f08
Eric <ericlg.link@gmail.com>
2022-10-11 19:50:44 -0700

    Need to render Mole and a container for the Mole

---
+++ src/Components/Mole.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import moleImage from '../mole.png'
+
+const Mole = (prop) => {
+    useEffect(() => {
+        
+    })
+}
---
+++ src/Components/MoleContainer.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+
+const moleContainer = (props) => {
+    let[theMole, setTheMole] = useState(false)
+
+}
