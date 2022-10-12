a004bc3fb5ab71983f7a7f48eb7cfa9ba6c28e2b
Eric <ericlg.link@gmail.com>
2022-10-11 19:53:49 -0700

    Need the mole on a timer

--- src/Components/Mole.js
+++ src/Components/Mole.js
@@ -3,6 +3,10 @@ import moleImage from '../mole.png'
 
 const Mole = (prop) => {
     useEffect(() => {
-        
+        let randomSeconds = Math.ceil(Math.random() * 10000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randomSeconds)
+        return () => clearTimeout(timer)
     })
 }
