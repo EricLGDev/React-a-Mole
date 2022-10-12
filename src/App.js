30f439c38cc6597fb211ef02356f63ccf7dacfc9
Eric <ericlg.link@gmail.com>
2022-10-11 20:18:36 -0700

    Define score and create Mole Hill

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import { useState } from "react";
 import MoleContainer from './Components/MoleContainer'
 
 function App() {
+  let [score, setScore] = useState(0)
 
+  const createMoleHill = () => {
+    let hills = []
+    for(let i = 0; i < 9; i++) {
+      hills.push(<MoleContainer key={i} setScore={setScore} score={score} />)
+    }
+    return (
+      <div>
+        { hills }
+      </div>
+    )
+  }
+  
 }
 
 export default App;
